6fde60a3b31a41fc06a7dd5e9bcb27e5560b34ee
nastiandreeva <nsatiandreeva@gmail.com>
2022-03-08 18:52:46 +0500

    Добавлено несколько функций

--- sandbox/src/main/java/ru/stqa/auto/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/auto/sandbox/MyFirstProgram.java
@@ -4,6 +4,20 @@ public class MyFirstProgram {
 
   public static void main(String[] args) {
     System.out.println("Hello, word");
+
+    double l = 5;
+    System.out.println("Площадь квадрата со стороной " + l + "=" + area(l));
+
+    double a = 4;
+    double b = 6;
+    System.out.println("Площадь прямоугольника со сторонами " + a + "и" + b + "=" + area(a,b));
   }
 
+  public static double area(double len){
+    return len + len;
+  }
+
+  public static double area(double a, double b){
+    return a * b;
+  }
 }
