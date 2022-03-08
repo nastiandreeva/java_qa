fecae92c1b83d0ff0bdb61342b8c37307cebfde7
nastiandreeva <nsatiandreeva@gmail.com>
2022-03-08 19:21:06 +0500

    Созданы классы и методы

--- sandbox/src/main/java/ru/stqa/auto/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/auto/sandbox/MyFirstProgram.java
@@ -5,19 +5,10 @@ public class MyFirstProgram {
   public static void main(String[] args) {
     System.out.println("Hello, word");
 
-    double l = 5;
-    System.out.println("Площадь квадрата со стороной " + l + "=" + area(l));
+    Square s = new Square(5);
+    System.out.println("Площадь квадрата со стороной " + s.l + " = " + s.area()); // Вызов метода из класса
 
-    double a = 4;
-    double b = 6;
-    System.out.println("Площадь прямоугольника со сторонами " + a + "и" + b + "=" + area(a,b));
-  }
-
-  public static double area(double len){
-    return len + len;
-  }
-
-  public static double area(double a, double b){
-    return a * b;
+    Rectangle r = new Rectangle(4,6);
+    System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + r.area());
   }
 }
---
+++ sandbox/src/main/java/ru/stqa/auto/sandbox/Square.java
@@ -0,0 +1,15 @@
+package ru.stqa.auto.sandbox;
+
+public class Square {
+
+  public double l;
+
+  public Square(double l){  // Конструктор
+    this.l = l;             // Атрибут и переменная
+  };
+
+  public double area(){
+    return this.l + this.l;
+  }
+
+}
