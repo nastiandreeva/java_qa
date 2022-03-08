aaf1260b6c48f047b9f209f5094230e15954135a
nastiandreeva <nsatiandreeva@gmail.com>
2022-03-08 17:11:22 +0500

    Изменение настройки форматирования

--- sandbox/src/main/java/ru/stqa/auto/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/auto/sandbox/MyFirstProgram.java
@@ -2,8 +2,8 @@ package ru.stqa.auto.sandbox;
 
 public class MyFirstProgram {
 
-		public static void main(String[] args){
-			System.out.println("Hello, word");
-		}
+  public static void main(String[] args) {
+    System.out.println("Hello, word");
+  }
 
 }
