b1036bc34b8d7d1b8718201f5de083e985681430
nastiandreeva <nsatiandreeva@gmail.com>
2022-03-09 18:08:57 +0500

    Добавлены расчеты площади квадрата и прямоугольника

--- sandbox/src/main/java/ru/stqa/auto/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/auto/sandbox/MyFirstProgram.java
@@ -1,14 +1,17 @@
 package ru.stqa.auto.sandbox;
 
+import ru.stqa.auto.homework.Point;
+
 public class MyFirstProgram {
 
   public static void main(String[] args) {
     System.out.println("Hello, word");
 
     Square s = new Square(5);
-    System.out.println("Площадь квадрата со стороной " + s.l + " = " + s.area()); // Вызов метода из класса
+    System.out.println("Площадь квадрата со стороной " + s.l + " = " + s.area());   // Вызов метода из класса
 
     Rectangle r = new Rectangle(4,6);
     System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + r.area());
+
   }
 }
