2635acbeeed9f279c998566e4a75cc78f8bd17af
nastiandreeva <nsatiandreeva@gmail.com>
2022-03-14 18:26:37 +0500

    Добавлены тесты на площадь и на расстояние между двумя точками

--- sandbox/src/test/java/ru/stqa/auto/distancePoint/Distance.java
+++ sandbox/src/test/java/ru/stqa/auto/distancePoint/Distance.java
@@ -6,10 +6,17 @@ import org.testng.annotations.Test;
 public class Distance {
 
     @Test
-    public void testDistanceMethod() {
+    public void testDistanceMethod1() {
       Point p1 = new Point(0, 1);
       Point p2 = new Point(2, -2);
       Assert.assertEquals(p1.distanceMethod(p2), 3.605551275463989);
+    }
+
+    @Test
+    public void testDistanceMethod2() {
+      Point p1 = new Point(3, -4);
+      Point p2 = new Point(-6, 5);
+      Assert.assertEquals(p1.distanceMethod(p2), 12.727922061357855);
     }
 
   }
--- sandbox/src/test/java/ru/stqa/auto/sandbox/SquareTests.java
+++ sandbox/src/test/java/ru/stqa/auto/sandbox/SquareTests.java
@@ -6,14 +6,26 @@ import org.testng.annotations.Test;
 public class SquareTests {
 
   @Test //аннотация
-  public void testAreaSquare() {
+  public void testAreaSquare1() {
     Square s = new Square(5);
     Assert.assertEquals(s.area(), 25.0); //вспомогательная функция для подробного описания ошибки
   }
 
   @Test
-  public void testAreaRectangle(){
+  public void testAreaSquare2() {
+    Square s = new Square(18);
+    Assert.assertEquals(s.area(), 324.0); //вспомогательная функция для подробного описания ошибки
+  }
+
+  @Test
+  public void testAreaRectangle1(){
     Rectangle r = new Rectangle(4, 6);
     Assert.assertEquals(r.area(), 24.0);
+  }
+
+  @Test
+  public void testAreaRectangle2(){
+    Rectangle r = new Rectangle(14, 3);
+    Assert.assertEquals(r.area(), 42.0);
   }
 }
