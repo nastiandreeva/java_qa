76937f0a51b616d141215f467e8b54fe27f2dc71
nastiandreeva <nsatiandreeva@gmail.com>
2022-03-26 15:15:25 +0500

    Оптимизированы переходы между страницами создания группы и создания контакта

--- addressbook-web-tests/src/test/java/ru/stqa/auto/addressbook/appmanager/NavigationHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/auto/addressbook/appmanager/NavigationHelper.java
@@ -11,6 +11,11 @@ public class NavigationHelper extends HelperBase{
   }
 
   public void goToGroupPage() {
+    if (isElementPresent(By.name("h1"))
+            && wd.findElement(By.tagName("h1")).getText().equals("Groups")
+            && isElementPresent(By.name("new"))){
+      return;
+    };
     click(By.linkText("groups"));
   }
 
@@ -19,6 +24,9 @@ public class NavigationHelper extends HelperBase{
   }
 
   public void goToHomePage() {
+    if (isElementPresent(By.id("maintable"))){
+      return;
+    }
     click(By.linkText("home"));
   }
 
