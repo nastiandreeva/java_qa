85cdc0626c6a3348d5806b62f57edb4e137bb37e
nastiandreeva <nsatiandreeva@gmail.com>
2022-04-01 14:18:26 +0500

    Реализован выбор конкретного элемента из списка на странице по обращению к индексу

--- addressbook-web-tests/src/test/java/ru/stqa/auto/addressbook/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/auto/addressbook/tests/GroupModificationTests.java
@@ -13,9 +13,9 @@ public class GroupModificationTests extends TestBase{
     if (! app.getGroupHelper().isThereAGroup()) {
       app.getGroupHelper().createGroup(new GroupData("test1", "test2", "test3"));
     }
-    app.getGroupHelper().selectGroup();
+    app.getGroupHelper().selectGroup(before - 1);
     app.getGroupHelper().initGroupModification();
-    app.getGroupHelper().fillGroupForm(new GroupData("test1mod", "test2mod", "test3mod"));
+    app.getGroupHelper().fillGroupForm(new GroupData("test1modif", "test2mod", "test3mod"));
     app.getGroupHelper().submitGroupModification();
     app.getGroupHelper().returnToGroupPage();
     int after = app.getGroupHelper().getGroupCount();
