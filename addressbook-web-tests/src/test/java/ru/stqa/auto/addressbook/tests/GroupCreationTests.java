f644f3eee5bf1be52866c537b8148776f3411e9b
nastiandreeva <nsatiandreeva@gmail.com>
2022-04-07 19:48:14 +0500

    Модифицирован тест создания группы с использованием сравнения compare

--- addressbook-web-tests/src/test/java/ru/stqa/auto/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/auto/addressbook/tests/GroupCreationTests.java
@@ -13,21 +13,30 @@ public class GroupCreationTests extends TestBase {
   public void testGroupCreation() {
     app.getNavigationHelper().goToGroupPage();
     List<GroupData> before = app.getGroupHelper().getGroupList(); //объявление переменной типа лист
-    GroupData group =  new GroupData("test1", null, null);
+    GroupData group =  new GroupData("test2", null, null);
     app.getGroupHelper().createGroup(group);
     List<GroupData> after = app.getGroupHelper().getGroupList();
     Assert.assertEquals(after.size(), before.size() + 1 );
 
-    int max = 0;
-    for (GroupData g: after){
-      if (g.getId() > max){
-        max = g.getId();
-      }
-    }
-    group.setId(max);
+//    1 способ сравнения с помощью лясбда выражения их способа 2, превращаем список в поток и ищем максимальный
+    group.setId(after.stream().max((o1, o2) -> Integer.compare(o1.getId(), o2.getId())).get().getId());
     before.add(group);
     Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));  //сортированное множество, все одинаковые имена схлопываются
   }
 
+//    2 способ сравнения с помощью вспомогательного объекта компоратора
+//    Comparator<? super GroupData> byId = new Comparator<GroupData>() { //локальная переменная "сравниватель"
+//      @Override
+//      public int compare(GroupData o1, GroupData o2) {
+//        return Integer.compare(o1.getId(), o2.getId());
+//      }
+//    };
 
+//    3 способ сравнения с использованием цикла
+//    int max = 0;
+//    for (GroupData g: after){
+//      if (g.getId() > max){
+//        max = g.getId();
+//      }
+//    }
 }
