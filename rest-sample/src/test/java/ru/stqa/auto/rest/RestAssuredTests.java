31caac2e9e106cbc4be763281551a105a31a5805
nastiandreeva <nsatiandreeva@gmail.com>
2022-10-10 19:53:52 +0500

    Получение статуса бага через rest

---
+++ rest-sample/src/test/java/ru/stqa/auto/rest/RestAssuredTests.java
@@ -0,0 +1,70 @@
+package ru.stqa.auto.rest;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
+import com.google.gson.reflect.TypeToken;
+import com.jayway.restassured.RestAssured;
+import org.apache.http.client.fluent.Executor;
+import org.apache.http.client.fluent.Request;
+import org.apache.http.message.BasicNameValuePair;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+import java.util.Set;
+
+import static org.testng.AssertJUnit.assertEquals;
+
+public class RestAssuredTests extends TestBase {
+
+  @BeforeClass
+  public void init() {
+    RestAssured.authentication = RestAssured.basic("288f44776e7bec4bf44fdfeb1e646490", "");
+  }
+
+  @Test
+  public void testCreateIssue() throws IOException {
+    Set<Issue> oldIssues = getIssues();
+    Issue newIssue = new Issue().withSubject("1 Test issue").withDescription("New test issue");
+    int issueId = createIssue(newIssue);
+    Set<Issue> newIssues = getIssues();
+    oldIssues.add(newIssue.withId(issueId));
+    assertEquals(newIssues, oldIssues);
+  }
+
+  @Test
+  public void testSkipCreateIssue() throws IOException {
+    if (isIssueOpen(2274) == true) {                               // если статус бага 2274 'open' то скипаем
+      skipIfNotFixed(2274);
+    } else {
+      Set<Issue> oldIssues = getIssues();
+      Issue newIssue = new Issue().withSubject("Test issue").withDescription("New test issue");
+      int issueId = createIssue(newIssue);
+      Set<Issue> newIssues = getIssues();
+      oldIssues.add(newIssue.withId(issueId));
+      assertEquals(newIssues, oldIssues);
+    }
+  }
+
+  private Set<Issue> getIssues() throws IOException {                                             // получение всех баг репортов
+    String json = RestAssured.get("https://bugify.stqa.ru/api/issues.json").asString();
+    JsonElement parsed = JsonParser.parseString(json);                                            // парсим json в переменную
+    JsonElement issues = parsed.getAsJsonObject().get("issues");                                  // получаем список только issues
+    return new Gson().fromJson(issues, new TypeToken<Set<Issue>>() {}.getType());                 // преобразовываем в множество объектов типа Issue
+  }
+
+  private Executor getExecutor() {                                                                // авторизация под токеном
+    return Executor.newInstance().auth("288f44776e7bec4bf44fdfeb1e646490", "");
+  }
+
+  private int createIssue(Issue newIssue) throws IOException {                                    // создание баг репорта с параметрами subject и description
+    String json = RestAssured.given()
+            .parameter("subject", newIssue.getSubject())
+            .parameter("description", newIssue.getDescription())
+            .post("https://bugify.stqa.ru/api/issues.json").asString();
+    JsonElement parsed = JsonParser.parseString(json);                                             // анализируем строку
+    return parsed.getAsJsonObject().get("issue_id").getAsInt();                                    // берем айди
+  }
+
+}
